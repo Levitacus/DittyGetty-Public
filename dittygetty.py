00801338c97e63b5c6aa8cf06e0948e2c9cfa8d4
patmatts <mattsp@sou.edu>
2018-05-07 21:04:37 -0700

    added dittygetty.py changes from previous commit

--- dittygetty.py
+++ dittygetty.py
@@ -1,28 +1,94 @@
 import click
+import os
+from config import Config
+from playlist import *
+from songInfo import *
+
+APP_NAME = 'dittygetty'
+CONFIG_NAME = 'config.json'
+
+pass_config = click.make_pass_decorator(Config, ensure=True)
 
 @click.group()
-def entry_point():
+@pass_config
+def entry_point(config):
     '''entry point to the cli'''
+    if not config.load():
+	config.save()
     pass #entry point to the cli
 
 @entry_point.group()
 def playlist():
     '''test'''
+    if not os.path.exists(click.get_app_dir(APP_NAME)):
+	os.makedirs(click.get_app_dir(APP_NAME))
     pass
     
-@playlist.group('modify')
-def playlist_modify():
-    pass
+
+
+#@playlist.group('modify')
+#def playlist_modify():
+#    pass
     
-@playlist_modify.command('add')
+@playlist.command('add')
 @click.argument('song')
 @click.argument('artist')
-@click.pass_obj
-def playlist_add(song, artist):
+@pass_config
+def playlist_add(config, song, artist):
     '''Adds a song to the current playlist'''
-    click.echo('Song: %s. Artist: %s. \n' %(song, artist))
+    song_obj = SongInfo(song, artist)
+
+    temp_playlist = Playlist()
+    try:
+        temp_playlist.to_playlist((config['playlist']))
+        #print playlist
+    except KeyError:
+        print "Hey"
     
+    temp_playlist.add(song_obj)
+
+    playlist_dict = temp_playlist.to_dict()
+    #print playlist_dict
+    config['playlist'] = playlist_dict
+    config.save()
+
+    click.echo('Song: %s. Artist: %s. \n' %(song, artist))
+
+@playlist.command('display')
+@pass_config
+def playlist_get_dir(config):
+    '''Displays current playlist'''
+    temp_playlist = Playlist()
+
+    try:
+        temp_playlist.to_playlist((config['playlist']))
+        #print playlist
+    except KeyError:
+        print "Hey"
+
+    for songs in temp_playlist.get():
+	print(songs.toString())
+
+@playlist.command('get_dir')
+@pass_config
+def playlist_get_dir(config):
+    '''Gets current app directory'''
+    print(click.get_app_dir(APP_NAME))
+    print(config.get('test', 'Does not exist'))
+
+@playlist.command('write_test')
+@pass_config
+def playlist_write_dir(config):
+    '''Writes test to current directory'''
+
+
+    config['test'] = 'it works!!!'
+    config.save()
+    #click.echo('saved info')
+
 entry_point.add_command(playlist)
+
+    
 
 if __name__ == '__main__':
     entry_point()
