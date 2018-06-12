20dde1382376ee5d1496f5d48e55f56176788205
Patrick Matts <patrickmatts@Patricks-MacBook-Air.local>
2018-06-12 16:07:30 -0700

    fixed requirements issue

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="dittygetty",
     version="1.0.0",
     py_modules=["dittygetty", "config", "playlist", "songInfo", "helperFuncs", "gmusic", "dg_gui", "daily_playlist", "npr", "scraper"],
-    install_requires=["Click", "gmusicapi", "beautifulsoup4", "lxml"],
+    install_requires=["Click", "gmusicapi", "bs4", "lxml"],
     
     entry_points="""
         [console_scripts]
